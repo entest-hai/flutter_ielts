f1539027e4253c70f72aaf8a59658270cf1ec438
Tran Minh Hai <hai@bio-rithm.com>
2021-02-15 16:25:35 +0700

    clean categoricalscrollview

--- lib/main.dart
+++ lib/main.dart
@@ -154,20 +154,6 @@ class _ReadingViewState extends State<ReadingView> {
     );
   }
 
-  Widget _OptionViewA(size, isHidden) {
-    return AnimatedOpacity(
-      opacity: 1,
-      duration: const Duration(microseconds: 200),
-      child: AnimatedContainer(
-        duration: const Duration(microseconds: 200),
-        width: size.width,
-        alignment: Alignment.topCenter,
-        height: isHidden ? 0 : size.height / 3.5,
-        child: CategoriesScroller(),
-      ),
-      );
-  }
-
   Widget _QuestionBar(isHidden) {
     return Container(
             color: Colors.cyan,
@@ -220,107 +206,6 @@ class _ReadingViewState extends State<ReadingView> {
     setState(() {
       isHidden = !isHidden;
     });
-  }
-}
-
-
-
-class CategoriesScroller extends StatelessWidget {
-  const CategoriesScroller();
-  @override 
-  Widget build(BuildContext context){
-    final double categoryHeight = MediaQuery.of(context).size.height * 0.30 - 50; 
-    return SingleChildScrollView(
-      physics: BouncingScrollPhysics(),
-      scrollDirection: Axis.horizontal,
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-        child: FittedBox(
-          fit: BoxFit.fill,
-          alignment: Alignment.topCenter,
-          child: Row(
-            children: [
-              Container(
-                width: 150,
-                margin: EdgeInsets.only(right: 20),
-                height: categoryHeight,
-                decoration: BoxDecoration(color: Colors.orange.shade400, borderRadius: BorderRadius.all(Radius.circular(20))),
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Most\nFavorites", style: TextStyle(fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        "20 Items",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                width: 150,
-                margin: EdgeInsets.only(right: 20),
-                height: categoryHeight,
-                decoration: BoxDecoration(color: Colors.blue.shade400, borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "Newest",
-                          style: TextStyle(fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          "20 Items",
-                          style: TextStyle(fontSize: 16, color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                width: 150,
-                margin: EdgeInsets.only(right: 20),
-                height: categoryHeight,
-                decoration: BoxDecoration(color: Colors.blue.shade400, borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "Newest",
-                          style: TextStyle(fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          "20 Items",
-                          style: TextStyle(fontSize: 16, color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
   }
 }
 
